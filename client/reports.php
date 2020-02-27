907b84fddd001bd8c41c5aa1d68ca1145da77fc5
pranavbhuchhada <pranavbhuchhada@gmail.com>
2020-02-27 10:39:33 +0530

    Hackathon Started

---
+++ client/reports.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>E - healthcard - Reports</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/mycustom.css" rel="stylesheet">
+
+</head>
+<?php require("header.php")?>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-1">
+        <h1 class="h3 mb-0 text-gray-800">Reports</h1>
+        <h1 class="h3 ml-auto text-gray-800">UID - 9385 8771 7682</h1>
+    </div>
+</div>
+<?php require("footer.php")?>
+<script>
+    $(".sidebar li").removeClass("active");
+    $(".sidebar li:eq(3)").addClass("active");
+</script>
